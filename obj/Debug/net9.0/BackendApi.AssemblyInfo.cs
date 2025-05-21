ea899ddf756b08096bdca86cdee1fcfed4d6c68b
ludkant <ludkant@gmail.com>
2025-05-21 12:52:09 -0500

    primer comit desde luffy1
    actualizo visual code

--- obj/Debug/net9.0/BackendApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c739c37b9f27ee80f2dcb95c9a0f41eae29666")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
